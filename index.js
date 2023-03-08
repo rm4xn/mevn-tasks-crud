1f9bd60433ee1a2eb3d7035966b12c524d5c1be0
Max Marvin Ramos Mancilla <86208765+rm4xn@users.noreply.github.com>
2023-03-08 23:26:51 +0000

    testing index

--- index.js
+++ index.js
@@ -1,33 +1,35 @@
-import mongoose from 'mongoose'
-import express from 'express'
-import morgan from 'morgan'
+// import mongoose from 'mongoose'
+// import express from 'express'
+// import morgan from 'morgan'
 
-import { dirname } from 'path'
-import { fileURLToPath } from 'url'
+// import { dirname } from 'path'
+// import { fileURLToPath } from 'url'
 
-import tasksRouter from './src/routes/tasks.js'
+// import tasksRouter from './src/routes/tasks.js'
 
-mongoose.set('strictQuery', true)
-mongoose
-.connect(
-  'mongodb+srv://rm4xn:D2ieJqmvSjwx2J0x@mevn-database.syx39d2.mongodb.net/test'
-  )
-  .then((db) => console.log('Database is connected.'))
-  .catch((err) => console.log(err))
+// mongoose.set('strictQuery', true)
+// mongoose
+// .connect(
+//   'mongodb+srv://rm4xn:D2ieJqmvSjwx2J0x@mevn-database.syx39d2.mongodb.net/test'
+//   )
+//   .then((db) => console.log('Database is connected.'))
+//   .catch((err) => console.log(err))
   
-const app = express()
+// const app = express()
 
-// Settings
-app.set('port', process.env.PORT || 3000)
+// // Settings
+// app.set('port', process.env.PORT || 3000)
 
-// Middlewares
-app.use(morgan('dev'))
-app.use(express.json())
+// // Middlewares
+// app.use(morgan('dev'))
+// app.use(express.json())
 
-// Routes
-app.use('/api/tasks', tasksRouter)
+// // Routes
+// app.use('/api/tasks', tasksRouter)
 
-// Static files
-app.use(express.static(dirname(fileURLToPath(import.meta.url)) + '/dist'))
+// // Static files
+// app.use(express.static(dirname(fileURLToPath(import.meta.url)) + '/dist'))
 
-app.listen(app.get('port'))
+// app.listen(app.get('port'))
+
+console.log('hola desde el index')
