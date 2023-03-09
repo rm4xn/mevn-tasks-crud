72c3bc9f915af86cc6af0cbe8f460241822f255e
Max Marvin Ramos Mancilla <86208765+rm4xn@users.noreply.github.com>
2023-03-09 00:29:35 +0000

    add api routes

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-// import mongoose from 'mongoose'
+import mongoose from 'mongoose'
 import express from 'express'
-// import morgan from 'morgan'
+import morgan from 'morgan'
 
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 
-// import tasksRouter from './src/routes/tasks.js'
+import tasksRouter from './src/routes/tasks.js'
 
-// mongoose.set('strictQuery', true)
-// mongoose
-// .connect(
-//   'mongodb+srv://rm4xn:D2ieJqmvSjwx2J0x@mevn-database.syx39d2.mongodb.net/test'
-//   )
-//   .then((db) => console.log('Database is connected.'))
-//   .catch((err) => console.log(err))
+mongoose.set('strictQuery', true)
+mongoose
+.connect(
+  'mongodb+srv://rm4xn:D2ieJqmvSjwx2J0x@mevn-database.syx39d2.mongodb.net/test'
+  )
+  .then((db) => console.log('Database is connected.'))
+  .catch((err) => console.log(err))
   
 const app = express()
 
@@ -21,15 +21,13 @@ const app = express()
 app.set('port', process.env.PORT || 3000)
 
 // Middlewares
-// app.use(morgan('dev'))
+app.use(morgan('dev'))
 app.use(express.json())
 
-// // Routes
-// app.use('/api/tasks', tasksRouter)
+// Routes
+app.use('/api/tasks', tasksRouter)
 
-// // Static files
+// Static files
 app.use(express.static(dirname(fileURLToPath(import.meta.url)) + '/public'))
 
 app.listen(app.get('port'))
-
-console.log('hola desde el index')
